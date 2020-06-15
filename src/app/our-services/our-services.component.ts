936d7d74557ba944a5a7fa48605e2d950996cc04
Jesus Alberto Coronado <jcoronado@e4gslab.com>
2020-06-15 16:21:57 -0500

    ajustes

--- src/app/our-services/our-services.component.ts
+++ src/app/our-services/our-services.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./our-services.component.sass']
 })
 export class OurServicesComponent implements OnInit {
-
+  public idService;
   constructor() { }
 
   ngOnInit() {
+    this.idService=parseInt(sessionStorage.getItem("itemProductID"));
+    console.log(this.idService);
   }
 
 }
