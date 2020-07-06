138063c907a5d106a195d221f5787e0e8e4ec596
Jesus Alberto Coronado <jcoronado@e4gslab.com>
2020-07-06 15:57:54 -0500

    ajustes de merge

--- src/app/autonomo/components/carousel-products/carousel-products.component.ts
+++ src/app/autonomo/components/carousel-products/carousel-products.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-carousel-products',
   templateUrl: './carousel-products.component.html',
-  styleUrls: ['./carousel-products.component.sass']
+  styleUrls: ['./carousel-products.component.sass'],
+  animations: [
+    trigger('EnterLeave', [
+      state('flyIn', style({ transform: 'translateY(0)' })),
+      transition(':enter', [
+        style({ transform: 'translateY(-100%)' }),
+        animate('0.5s 300ms ease-in')
+      ]),
+      transition(':leave', [
+        animate('0.3s ease-out', style({ transform: 'translateY(100%)' }))
+      ])
+    ])
+  ]
 })
 export class CarouselProductsComponent implements OnInit {
 
