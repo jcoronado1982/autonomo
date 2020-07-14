7d04d8890c8fe2ed5284e83c6f4dbe19e0e60698
Jesus Alberto Coronado <jcoronado@e4gslab.com>
2020-07-14 10:56:26 -0500

    nueva fuente

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -40,9 +40,62 @@ export class HomeComponent implements OnInit {
   public nameSend;
   public gui;
   public cr='';
+  public prodList = [];
   constructor(private router:Router,public appComponent:AppComponent,private services: Service,private loadingScreenService:LoadingScreenService,private global:GlobalService, public dialog:MatDialog){}
   
   ngOnInit() {
+    let imgBan = [
+      {
+        name: "KOTLIN",
+        imgAdj: "../../../../assets/images/kotlin.png",
+        nameofclass:"py-icon",
+        paddingIcon:"65px 0px"
+      },
+      {
+        name: "PYTHON",
+        imgAdj: "../../../../assets/images/pyLogo.png",
+        nameofclass:"py-icon",
+        paddingIcon:"65px 0px"
+      },
+      {
+        name: "CSHARP",
+        imgAdj: "../../../../assets/images/csharpLogo.png",
+        nameofclass:"py-icon",
+        paddingIcon:"65px 0px"
+      },
+      {
+        name: "ANGULAR",
+        imgAdj: "../../../../assets/images/angularLogo.png",
+        nameofclass:"py-icon",
+        paddingIcon:"65px 0px"
+      },
+      {
+        name: "REACT",
+        imgAdj: "../../../../assets/images/reactLogo.png",
+        nameofclass:"py-icon",
+        paddingIcon:"65px 0px"
+      },
+      {
+        name: "SQL",
+        imgAdj: "../../../../assets/images/sqlSLogo.png",
+        nameofclass:"py-icon",
+        paddingIcon:"65px 0px"
+      },
+      {
+        name: "GCP",
+        imgAdj: "../../../../assets/images/gcpLogo.png",
+        nameofclass:"py-icon",
+        paddingIcon:"65px 0px"
+      },      
+      {
+        name: "AWS",
+        imgAdj: "../../../../assets/images/awsNewLogo.png",
+        nameofclass:"py-icon",
+        paddingIcon:"65px 0px"
+      }      
+      
+    ];
+    this.prodList = imgBan;
   }
 
   ngAfterViewInit(){
