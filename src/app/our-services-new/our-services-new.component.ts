8f13ffa0b3ab7c1cf083b3f987a3340fae75e351
Jesus Alberto Coronado <jcoronado@e4gslab.com>
2020-07-21 09:22:04 -0500

    ajustes

--- src/app/our-services-new/our-services-new.component.ts
+++ src/app/our-services-new/our-services-new.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LanguageService } from '../language.service';
-
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { GlobalService } from '../global.service';
 @Component({
   selector: 'app-our-services-new',
   templateUrl: './our-services-new.component.html',
@@ -9,13 +10,44 @@ import { LanguageService } from '../language.service';
 })
 export class OurServicesNewComponent implements OnInit {
   public idService;
-  constructor(private language: LanguageService, private router:Router) { }
+  orderForm: FormGroup;
+  constructor(private global: GlobalService, private formBuilder: FormBuilder, private language: LanguageService, private router: Router) {
+    this.createForm();
+  }
 
   ngOnInit(): void {
     window.scroll(0, 0);
-    this.idService=localStorage.getItem("idItem");
+    this.idService = localStorage.getItem("idItem");
   }
-  backHome(){
+  private createForm() {
+    this.orderForm = this.formBuilder.group({
+      nameClient: ['', Validators.required],
+      emailClient: ['', Validators.required],
+      projectType: ['', Validators.required],
+      projectDescription: ['', Validators.required]
+    });
+  }
+  backHome() {
     this.router.navigate(['home']);
+  }
+  sentForm() {
+    if (this.orderForm.value.nameClient == '') {
+      this.global.notif(this.language.content.contact.right.nameAdvice);
+    }
+    else if (this.orderForm.value.emailClient == '') {
+      this.global.notif(this.language.content.contact.right.emailAdvice);
+    }
+    else if (this.global.validarEmail(this.orderForm.value.emailClient) == false) {
+      this.global.notif(this.language.content.contact.right.emailInvalidAdvice);
+    }
+    else if (this.orderForm.value.projectType == '') {
+      this.global.notif(this.language.content.contact.right.typeAdvice);
+    }
+    else if (this.orderForm.value.projectDescription == '') {
+      this.global.notif(this.language.content.contact.right.descriptionAdvice);
+    }
+    else {
+      this.global.notif(this.language.content.contact.right.sentAdvice);
+    }
   }
 }
